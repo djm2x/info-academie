d71f8674990b71590055abe8d5776cf687576b18
djm2x <dj-m2x@hotmail.com>
2020-09-26 01:45:42 +0100

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,47 +8,6 @@ use Session;
 
 class AppServiceProvider extends ServiceProvider
 {
-    protected $cntsh = [
-        //1cntsh
-        ['route' => 'cntsh/diagramme', 'name' => 'Diagramme'],
-        ['route' => 'cntsh/missions', 'name' => 'Missions'],
-        ['route' => 'cntsh/directeur', 'name' => 'Motdudirecteur'],
-        ['route' => 'cntsh/reseau', 'name' => 'Reseaunational'],
-        ['route' => 'cntsh/gouvernance', 'name' => 'Gouvernance'],
-        ['route' => 'cntsh/histoire', 'name' => 'Histoire'],
-        ['route' => 'cntsh/bilan', 'name' => 'Bilanactivite'],
-    ];
-    protected $sang = [
-        //2sang
-        ['route' => 'sang/quoi', 'name' => 'Lesangestquoi'],
-        ['route' => 'sang/produit', 'name' => 'Produitsanguins'],
-        ['route' => 'sang/hemovigilance', 'name' => 'Hemouigilance'],
-        ['route' => 'sang/cout', 'name' => 'Coutdeprestation'],
-        ['route' => 'sang/situation', 'name' => 'Situationnationalededondesang'],
-        ['route' => 'sang/audit', 'name' => 'Audit'],
-    ];
-    protected $partenaires = [
-        //4Partenaires
-        ['route' => 'partenaires/association', 'name' => 'Associationdonneurs'],
-        ['route' => 'partenaires/partenaires', 'name' => 'Partenairesinstitutionnels'],
-        ['route' => 'partenaires/transfusion', 'name' => 'Transfusionmondiale'],
-        ['route' => 'partenaires/press', 'name' => 'Press'],
-    ];
-    protected $don = [
-        //3don du sang
-        ['route' => 'planification', 'name' => 'Planification'],
-        ['route' => 'don-du-song/Pourquoi', 'name' => 'Pouquoidonnerdusang'],
-        ['route' => 'don-du-song/qui', 'name' => 'Quipeutdeonnerdusang'],
-        ['route' => 'don-du-song/informations', 'name' => 'Informationspostdon'],
-        ['route' => 'don-du-song/promesse', 'name' => 'Faireunepromessededon'],
-        ['route' => 'don-du-song/ou-donner', 'name' => 'Oudonnersonsang'],
-        ['route' => 'don-du-song/faq', 'name' => 'FAQ'],
-
-        //  ['route' => 'president', 'name' => 'Nos Activiés'],
-        //  ['route' => 'president', 'name' => 'Galerie'],
-        //  ['route' => 'president', 'name' => 'Contact'],
-
-    ];
     /**
      * Register any application services.
      *
@@ -67,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        // URL::forceScheme('https');
+        URL::forceScheme('https');
 
         // view()->share('cntsh', $this->cntsh);
         // view()->share('sang', $this->sang);
