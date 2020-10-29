d25aeae42ea1c5e3a6917c5819857afc98a21499
djm2x <dj-m2x@hotmail.com>
2020-10-29 22:28:45 +0100

    commit

--- angular/src/app/admin/dash/chat/chat.hub.service.ts
+++ angular/src/app/admin/dash/chat/chat.hub.service.ts
@@ -3,6 +3,9 @@ import { Subject } from 'rxjs';
 import { Message } from 'src/app/models/models';
 import { SessionService } from 'src/app/shared';
 import Echo from 'laravel-echo';
+import {PusherConnector} from 'laravel-echo/dist/connector';
+
+
 import Pusher from 'pusher-js';
 (window as any).Pusher = Pusher;
 
@@ -32,33 +35,52 @@ export class ChatHubService {
       wsHost: this.hubUrl.replace('https://', '').replace('http://', '').replace(':8000', ''),
       wsPort: 6001,
       forceTLS: false,
-      encrypted: true,
+      encrypted: false,
       cluster: 'mt1',
-      enabledTransports: ['ws', 'wss'],
+      enabledTransports: ['ws'/*, 'wss'*/],
       disableStats: true,
     });
 
-    this.echo.connect();
+    // this.echo.connect();
+
+    // console.warn(this.echo)
 
     return this;
   }
 
   public startConnection(): void {
+
+    const connector: PusherConnector = this.echo.connector;
     // this.echo.private(`App.User.${this.session.user.id}`).listen('MessageEvent', r => {
     //   console.log(r);
     // });
+    connector.connect();
 
-    this.echo.listen(`private.${this.session.user.id}`, 'MessageEvent' , r => {
-      console.warn('fanaly its works  echo.listen');
-    });
-
-    // this.echo.private(`private.${this.session.user.id}`).listen('MessageEvent', r => {
-    //   console.log(r);
+    // connector.privateChannel(`private.${this.session.user.id}`).listen('MessageEvent', r => {
+    //   console.warn('fanaly its works  echo.channel');
     // });
 
-    this.echo.channel(`private.${this.session.user.id}`).listen('MessageEvent', r => {
-      console.warn('fanaly its works  echo.channel');
+    connector.channel(`users.${this.session.user.id}`).listen('MessageEvent', r => {
+      console.warn('fanaly its works  channel(`users');
     });
+
+    // connector.presenceChannel(`private.${this.session.user.id}`).here( r => {
+    //   console.warn('fanaly its works  echo.channel');
+    // });
+
+    // // console.warn(connector)
+
+    // this.echo.listen(`private.${this.session.user.id}`, 'MessageEvent' , r => {
+    //   console.warn('fanaly its works  echo.listen');
+    // });
+
+    // // this.echo.private(`private.${this.session.user.id}`).listen('MessageEvent', r => {
+    // //   console.log(r);
+    // // });
+
+    // this.echo.channel(`private.${this.session.user.id}`).listen('MessageEvent', r => {
+    //   console.warn('fanaly its works  echo.channel');
+    // });
 
     this.echo.channel(`users.${this.session.user.id}`).listen('MessageEvent', r => {
       this.newMessage.next(r.message);
