56738a6be7a3bb78a5ba5bd962ea80b54ed3fee5
djm2x <dj-m2x@hotmail.com>
2021-01-17 03:28:05 +0100

    commit

--- angular/src/app/admin/dash/cours/detail/video/video.component.ts
+++ angular/src/app/admin/dash/cours/detail/video/video.component.ts
@@ -7,13 +7,19 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./video.component.scss']
 })
 export class VideoComponent implements OnInit {
-
-  constructor(public dialogRef: MatDialogRef<any>, @Inject(MAT_DIALOG_DATA) public data: {lien: string}) { }
+  lien = this.data.lien.replace('https://www.youtube.com/watch?v=', '');
+  constructor(public dialogRef: MatDialogRef<any>, @Inject(MAT_DIALOG_DATA) public data: { lien: string }) { }
 
   ngOnInit(): void {
+    // const l = this.data.lien;
+    // this.lien = l.substring(
+    //   l.lastIndexOf('watch?v=') + 1
+    // );
+    // console.log(this.data.lien)
+    // console.log(this.lien)
     const tag = document.createElement('script');
 
-    tag.src = "https://www.youtube.com/iframe_api";
+    tag.src = 'https://www.youtube.com/iframe_api';
     document.body.appendChild(tag);
   }
 
